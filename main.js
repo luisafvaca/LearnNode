ec7d8e9b692c37e78003f898a90941461167fcac
Luisa Vaca <luisafernanda9111@gmail.com>
2020-04-29 15:16:35 -0500

    added chalk

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-var name = process.argv[2];
-console.log(`Hola ${name}`);
+var chalk = require('chalk');
+
+var red = chalk.bold.red;
+var green = chalk.bgBlack.cyan;
+
+console.log(red('Something went wrong'));
+console.log(green('Yey! you doing good'));
