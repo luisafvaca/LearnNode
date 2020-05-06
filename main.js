c91518557d82e5d39758a9a275c6cda3704e2435
Luisa Vaca <luisafernanda9111@gmail.com>
2020-05-06 10:40:55 -0500

    added my first module and export a simple string

--- main.js
+++ main.js
@@ -1,14 +1,2 @@
-var fs = require('fs');
-var chalk = require('chalk');
-
-var red = chalk.bold.redBright;
-var blue = chalk.bold.blue;
-var green = chalk.green;
-
-//Llamados a archivos de manera asincrona
-
-fs.readFile('luisa2.txt', (err, data) =>{
-    if(err) throw green(err);
-    console.log(red('imprimiendo resultado'));
-    console.log(blue(data.toString()));
-});
+var module = require('./mymodule');
+console.log(module);
