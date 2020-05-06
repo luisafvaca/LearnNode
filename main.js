d9b0d14972db42a312dda3e834c05536c18fc9e2
Luisa Vaca <luisafernanda9111@gmail.com>
2020-05-06 10:24:16 -0500

    added a unexisted file to get an error

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ var chalk = require('chalk');
 
 var red = chalk.bold.redBright;
 var blue = chalk.bold.blue;
+var green = chalk.green;
 
 //Llamados a archivos de manera asincrona
 
-fs.readFile('luisa.txt', (err, data) =>{
-    if(err) throw err;
+fs.readFile('luisa2.txt', (err, data) =>{
+    if(err) throw green(err);
     console.log(red('imprimiendo resultado'));
     console.log(blue(data.toString()));
 });
