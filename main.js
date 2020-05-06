17600674079c5d7f9e11daaea9aff90cc1f4bb9a
Luisa Vaca <luisafernanda9111@gmail.com>
2020-05-06 10:14:48 -0500

    read file async

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 var fs = require('fs');
-var resultado = fs.readFileSync('luisa.txt');
+var chalk = require('chalk');
 
-console.log(resultado.toString());
+var red = chalk.bold.redBright;
+var blue = chalk.bold.blue;
+
+//Llamados a archivos de manera asincrona
+
+fs.readFile('luisa.txt', (err, data) =>{
+    if(err) throw err;
+    console.log(red('imprimiendo resultado'));
+    console.log(blue(data.toString()));
+});
